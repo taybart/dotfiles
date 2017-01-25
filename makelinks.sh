a06a839ebaa74f42ad85a122a139e8a06eb9f30d
Taylor <taybart@gmail.com>
2017-01-24 17:16:23 -0700

    [MAKELINKS] Added tmux.conf to makelinks script

--- makelinks.sh
+++ makelinks.sh
@@ -3,3 +3,4 @@ ln -s $HOME/.dotfiles/zsh $HOME/.zsh
 ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/vim $HOME/.vim
 ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
