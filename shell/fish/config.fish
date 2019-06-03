58715f618dc43afa6d75446e4455ae9d13082568
Taylor Bartlett <taybart@gmail.com>
2019-06-03 17:25:00 -0600

    Cleanup

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,8 +1,17 @@
 # -- Aliases --
 alias q="exit"
 alias :q="exit"
-alias zshrc="nvim ~/.zshrc && . ~/.zshrc"
 alias startredis="docker run -p 6379:6379 -d redis"
+
+alias top="gotop -m"
+alias ls="ls --color -l -h"
+alias update="echo please define"
+alias install="echo please define"
+
+# Python
+alias python="python3"
+alias pip="pip3"
+alias grep="grep -RIns --color=auto --exclude=\"tags\""
 
 
 # Git Aliases
@@ -35,32 +44,13 @@ set -x PATH $PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools:$ANDROID_HOME/plat
 set -x PATH "$PATH:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"
 
 
-############### Alias ##################
-set HOMEIP "192.168.0.100"
-set EXHOMEIP "73.229.143.233"
-alias home="title home && ssh $EXHOMEIP -p 12355"
-alias localhome="title localhome && ssh $HOMEIP"
-
-alias top="gotop -m"
-alias ls="ls -G -l -h"
-alias lsusb="system_profiler SPUSBDataType"
-alias showhidden="defaults write com.apple.finder AppleShowAllFiles"
-alias ctags="`brew --prefix`/bin/ctags"
-alias update="brew update && brew upgrade"
-alias install="brew install"
-
-# Python
-alias python="python3"
-alias pip="pip3"
 
 
 
 ############## Fucntions ###############
 
-#alias grep="grep -RIns --color=auto --exclude=\"tags\""
-#
-bind \ee echo 'hello'
-#
+
+
 bind ! __history_previous_command
 bind '$' __history_previous_command_arguments
 function __history_previous_command
