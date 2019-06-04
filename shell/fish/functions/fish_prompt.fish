84796c4b446400df431e4fcc66d3489b3ba4a07e
Taylor <taybart@gmail.com>
2019-06-04 11:05:37 -0600

    Fix dirty git prompt to be pretty

--- shell/fish/functions/fish_prompt.fish
+++ shell/fish/functions/fish_prompt.fish
@@ -35,7 +35,7 @@ function fish_prompt
     set -l git_branch (_git_branch_name)
 
     if [ (_git_is_dirty) ]
-      set git_info '(' $yellow $git_branch "*" $normal ')'
+      set git_info '(' $yellow $git_branch $red ' ✗' $normal ')'
     else
       set git_info '(' $green $git_branch $normal ')'
     end
