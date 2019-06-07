9e76f84a77af36853e3e5b67a96ef6ca7b31e1ff
Taylor <taybart@gmail.com>
2019-06-06 19:51:42 -0600

    Idk

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,75 +1,76 @@
 if status --is-interactive
-# -- Aliases --
-alias q="exit"
-alias :q="exit"
-alias startredis="docker run -p 6379:6379 -d redis"
+  # -- Aliases --
+  alias q="exit"
+  alias :q="exit"
+  alias startredis="docker run -p 6379:6379 -d redis"
 
-alias top="gotop -m"
-alias ls="ls --color -l -h"
-alias update="echo please define"
-alias install="echo please define"
+  alias top="gotop -m"
+  alias ls="ls --color -l -h"
+  alias update="echo please define"
+  alias install="echo please define"
 
-# Python
-alias python="python3"
-alias pip="pip3"
-alias grep="grep -RIns --color=auto --exclude=\"tags\""
-
-
-# Git Aliases
-alias gs="git status"
-alias gcm="git commit -m"
-alias gd="git diff --patience --ignore-space-change"
-alias gpo=" git pull origin"
-alias gpom=" git pull origin master"
-alias gitadddeleted="git ls-files --deleted -z | xargs -0 git rm"
+  # Python
+  alias python="python3"
+  alias pip="pip3"
+  alias grep="grep -RIns --color=auto --exclude=\"tags\""
 
 
-############## Variables ###############
+  # Git Aliases
+  # abbr -a gs 'git status'
+  alias gs="git status"
+  alias gcm="git commit -m"
+  alias gd="git diff --patience --ignore-space-change"
+  alias gpo=" git pull origin"
+  alias gpom=" git pull origin master"
+  alias gitadddeleted="git ls-files --deleted -z | xargs -0 git rm"
 
-set fish_greeting
 
-set -x EDITOR nvim
-# set -x LC_ALL en_US.UTF-8
-set GPG_TTY (tty)
+  ############## Variables ###############
 
-# Go
+  set fish_greeting
+
+  set -x EDITOR nvim
+  # set -x LC_ALL en_US.UTF-8
+  set GPG_TTY (tty)
+
+  # Go
   set -x GOPATH $HOME/dev/.go
   set -x PATH $HOME/dev/.go/bin:$PATH
 
-# Android
+  # Android
   set -x ANDROID_HOME $HOME/Library/Android/sdk
   set -x PATH $PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
-# Node
+  # Node
   set -x PATH "$PATH:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"
 
-############## Functions ###############
+  ############## Functions ###############
 
   bind ! __history_previous_command
   bind '$' __history_previous_command_arguments
   function __history_previous_command
-switch (commandline -t)
-  case "!"
+    switch (commandline -t)
+case "!"
   commandline -t $history[1]; commandline -f repaint
-  case "*"
+case "*"
   commandline -i !
-  end
-  end
+end
+end
 
-  function __history_previous_command_arguments
-switch (commandline -t)
-  case "!"
+function __history_previous_command_arguments
+  switch (commandline -t)
+case "!"
   commandline -t ""
   commandline -f history-token-search-backward
-  case "*"
+case "*"
   commandline -i '$'
-  end
-  end
+end
+end
 
-  theme_gruvbox dark
+theme_gruvbox dark
 
-  set -l local $HOME/.local.fish
-  if test -e $local
+set -l local $HOME/.local.fish
+if test -e $local
   source $local
-  end
-  end
+end
+end
