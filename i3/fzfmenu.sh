2a88f3a262ca99abd4b1dc2374758a8d41688ec6
Taylor <taybart@gmail.com>
2021-07-29 19:50:01 -0600

    update fzfmenu to use taybart/launch

--- i3/fzfmenu.sh
+++ i3/fzfmenu.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env zsh
 
-kitty --name 'fzfmenu' -- /home/taylor/.i3/getexe
+kitty --name 'fzfmenu' \
+  -o remember_window_size=no \
+  -o initial_window_width=400 \
+  -o initial_window_height=400 \
+  -o window_border_width=20pt \
+  -o hide_window_decorations=no \
+  -o window_padding_width=10 \
+  -- /home/taylor/.i3/getexe
