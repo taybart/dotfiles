7bad7fb3c000e1302a1aebfcea7b469bb20392b1
Taylor Bartlett <taybart@gmail.com>
2021-09-09 18:13:11 -0600

    feat: add SkyRocket

--- macos/hammerspoon/init.lua
+++ macos/hammerspoon/init.lua
@@ -1,22 +1,28 @@
 require('volume')
 require('brightness')
-require('resize_windows')
-require('drag_windows')
--- require('snap_windows')
+require('snap_windows')
+
+local SkyRocket = hs.loadSpoon("SkyRocket")
+SkyRocket:new({
+  noClick = true,
+  moveModifiers = {'cmd', 'ctrl'},
+  resizeModifiers = {'alt', 'ctrl'},
+})
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "r", hs.reload)
 
--- local function reload_config(files)
---     local do_reload = false
---     for _,file in pairs(files) do
---         if file:sub(-4) == ".lua" then
---             do_reload = true
---         end
---     end
---     if do_reload then
---         hs.reload()
---     end
--- end
--- hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+local function reload_config(files)
+    local do_reload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            do_reload = true
+        end
+    end
+    if do_reload then
+        hs.reload()
+    end
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
 
 
 hs.alert.show("Config loaded")
