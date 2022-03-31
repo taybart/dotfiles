d870a4710d1740b977468bfa0ce3565e70d667a9
Taylor Bartlett <taybart@gmail.com>
2022-03-31 17:28:34 -0600

    chore: remove hammerspoon

--- macos/hammerspoon/Spoons/EmmyLua.spoon/annotations/hs.spaces.lua
+++
@@ -1,8 +0,0 @@
---# selene: allow(unused_variable)
----@diagnostic disable: unused-local
-
--- Controls for macOS Spaces. Currenly only used by `hs.spaces.watcher`. 
----@class hs.spaces
-local M = {}
-hs.spaces = M
-
