ab0925cbdf5bffa377e2d47e2a898d8d170676a6
Taylor Bartlett <taybart@gmail.com>
2022-07-15 17:21:22 -0600

    feat: better resting

--- nvim/lua/lsp/rest.lua
+++ nvim/lua/lsp/rest.lua
@@ -13,7 +13,7 @@ local surrogate_language = 'hcl'
 local ft_to_parser = parsers.filetype_to_parsername
 ft_to_parser.rest = surrogate_language
 
-local function get_requests()
+local function exec_block_under_cursor()
   local query = [[(block (identifier) @requests (#eq? @requests "request")) @block]]
   local success, parsed_query = pcall(function()
     return vim.treesitter.parse_query(surrogate_language, query)
@@ -43,9 +43,50 @@ local function get_requests()
     end)
   end
   if not did_execute then
-    print('error: no block under cursor')
+    print('no block under cursor')
   end
 end
+
+function M.popup_rest()
+  local Popup = require('nui.popup')
+  local event = require('nui.utils.autocmd').event
+
+  local popup = Popup({
+    enter = true,
+    focusable = true,
+    border = {
+      style = 'rounded',
+    },
+    position = '50%',
+    size = {
+      width = '80%',
+      height = '60%',
+    },
+    buf_options = {
+      buflisted = false,
+    },
+  })
+
+  -- mount/open the component
+  popup:mount()
+
+  popup:on({ event.BufWinLeave }, function()
+    vim.schedule(function()
+      vim.cmd('bd')
+      popup:unmount()
+    end)
+  end, { once = true })
+  -- unmount component when cursor leaves buffer
+  popup:on(event.BufLeave, function()
+    vim.cmd('bd')
+    popup:unmount()
+  end)
+
+  vim.cmd('e client.rest')
+  -- set content
+  -- vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, { 'Hello World' })
+end
+vim.api.nvim_create_user_command('OpenRestClient', M.popup_rest, {})
 
 require('utils').create_augroups({
   rest_lsp = {
@@ -60,24 +101,29 @@ require('utils').create_augroups({
       event = 'FileType',
       pattern = 'rest',
       callback = function()
+        vim.api.nvim_create_user_command('ExecuteFile', M.execute_file, {})
         vim.api.nvim_create_user_command('ExecuteBlock', M.execute_block, { nargs = '?' })
 
         maps.mode_group('n', {
-          { '<c-E>', ':' .. rest_cmd .. ' -f %<cr>' },
           { '<c-e>', ':ExecuteBlock<cr>' },
+          -- { '<c-E>', ':ExecuteFile<cr>' },
         })
       end,
     },
   },
 })
 
+function M.execute_file()
+  vim.cmd(rest_cmd .. [[ -f % ]])
+end
 function M.execute_block(args)
   local label = args.fargs[1]
+  -- execute the label if given
   if label ~= '' then
     vim.cmd(rest_cmd .. [[ -f % -l ]] .. label)
     return
   end
-  get_requests()
+  exec_block_under_cursor()
 end
 
 return M
