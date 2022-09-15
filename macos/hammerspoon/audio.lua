06585c34d7ad02220c335f2d6035241cbd579817
Taylor Bartlett <taybart@gmail.com>
2022-09-15 12:32:32 -0600

    fix: better audio switcher in hammerspoon

--- macos/hammerspoon/audio.lua
+++ macos/hammerspoon/audio.lua
@@ -1,14 +1,26 @@
 hs.hotkey.bind({ 'cmd', 'alt', 'ctrl' }, 'a', function()
   local source = 'taps'
-  local success = hs.audiodevice.findOutputByName(source):setDefaultOutputDevice()
+  local output = hs.audiodevice.findOutputByName(source)
+  if output == nil then
+    hs.alert.show('could not find output device')
+    return
+  end
+  local success = output:setDefaultOutputDevice()
   if not success then
     hs.alert.show('could not set output device')
     return
   end
-  success = hs.audiodevice.findInputByName(source):setDefaultInputDevice()
+  success = output:setDefaultEffectDevice()
   if not success then
-    hs.alert.show('could not set input device')
+    hs.alert.show('could not set output device')
     return
   end
-  hs.alert.show('switched to output to ' .. source)
+  local input = hs.audiodevice.findInputByName(source)
+  if input ~= nil then
+    success = input:setDefaultInputDevice()
+    if not success then
+      hs.alert.show('could not set input device')
+    end
+  end
+  hs.alert.show('switched output to ' .. source)
 end)
