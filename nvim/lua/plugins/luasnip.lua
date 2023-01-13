a9404a3dc2da598e29be1090c03a2d82a36ee6c2
Taylor Bartlett <taybart@gmail.com>
2023-01-12 17:54:33 -0700

    refact: can't save space don't waste it

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,21 +1,20 @@
 return {
   'L3MON4D3/LuaSnip',
   config = function()
-    local ls = require('luasnip')
     require('snippets').setup()
     require('utils/maps').group({ silent = true }, {
       {
         'i',
         '<c-e>',
         function()
-          ls.jump(1)
+          require('luasnip').jump(1)
         end,
       },
       {
         's',
         '<c-e>',
         function()
-          ls.jump(1)
+          require('luasnip').jump(1)
         end,
       },
     })
