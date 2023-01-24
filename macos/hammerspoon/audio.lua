3f658ef9b03c0fa9b178a4e99b6c9f17edd8cd0c
Taylor Bartlett <taybart@gmail.com>
2023-01-24 12:28:19 -0700

    feat: add macbook pro speaker output key combo

--- macos/hammerspoon/audio.lua
+++ macos/hammerspoon/audio.lua
@@ -1,5 +1,32 @@
-hs.hotkey.bind({ 'cmd', 'alt', 'ctrl' }, 'a', function()
+hs.hotkey.bind({ 'cmd', 'ctrl', 'shift' }, 'a', function()
   local source = 'TAPs'
+  local output = hs.audiodevice.findOutputByName(source)
+  if output == nil then
+    hs.alert.show('could not find output device')
+    return
+  end
+  local success = output:setDefaultOutputDevice()
+  if not success then
+    hs.alert.show('could not set output device')
+    return
+  end
+  success = output:setDefaultEffectDevice()
+  if not success then
+    hs.alert.show('could not set output device')
+    return
+  end
+  local input = hs.audiodevice.findInputByName(source)
+  if input ~= nil then
+    success = input:setDefaultInputDevice()
+    if not success then
+      hs.alert.show('could not set input device')
+    end
+  end
+  hs.alert.show('switched output to ' .. source)
+end)
+
+hs.hotkey.bind({ 'cmd', 'alt', 'ctrl' }, 'a', function()
+  local source = 'MacBook Pro Speakers'
   local output = hs.audiodevice.findOutputByName(source)
   if output == nil then
     hs.alert.show('could not find output device')
