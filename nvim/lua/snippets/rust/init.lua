3f125711f70cff4d485b70d31e43cb2a742c0920
Taylor Bartlett <taybart@gmail.com>
2023-08-21 21:30:09 -0600

    feat: add `getip` and read rust snippet

---
+++ nvim/lua/snippets/rust/init.lua
@@ -0,0 +1,17 @@
+local ls = require('luasnip')
+local s = ls.s
+local i = ls.insert_node
+local fmt = require('luasnip.extras.fmt').fmt
+
+local snippets = {
+  s(
+    { trig = 'read', name = 'read_file', dscr = 'Read file to string' },
+    fmt([[let data = fs::read_to_string("{}").expect("Unable to read file");]], {
+      i(1, 'value'),
+    })
+  ),
+}
+
+local autosnippets = {}
+
+return snippets, autosnippets
