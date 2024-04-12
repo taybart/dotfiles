f061e2d155390bc53b4798c1a634e802cb382307
Taylor Bartlett <taybart@gmail.com>
2024-04-12 16:00:57 -0600

    fix: remove unused keymap

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -57,7 +57,6 @@ return {
 
   {
     'ThePrimeagen/harpoon',
-    -- '~/.tmp/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
@@ -67,14 +66,14 @@ return {
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():add()
       end)
-      -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set('n', '<C-S-P>', function()
-        harpoon:list():prev()
-      end)
-      vim.keymap.set('n', '<C-S-N>', function()
-        harpoon:list():next()
-      end)
-      vim.keymap.set('n', '<C-e>', function()
+      -- -- Toggle previous & next buffers stored within Harpoon list
+      -- vim.keymap.set('n', '<c-s-p>', function()
+      --   harpoon:list():prev()
+      -- end)
+      -- vim.keymap.set('n', '<c-s-n>', function()
+      --   harpoon:list():next()
+      -- end)
+      vim.keymap.set('n', '<c-e>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end)
 
