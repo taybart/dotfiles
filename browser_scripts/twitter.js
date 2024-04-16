46270090e263daa9475ceab14681c3eb20ff1016
Taylor Bartlett <taybart@gmail.com>
2024-04-16 16:21:26 -0600

    feat: twitter ad block

--- browser_scripts/twitter.js
+++ browser_scripts/twitter.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Twitter remove right sidebar
+// @name         twitter
 // @version      0.0.1
 // @namespace    https://twitter.com
 // @description  fixes twitter ui
@@ -10,7 +10,7 @@
 // @grant        none
 // ==/UserScript==
 
-;(() => {
+; (() => {
   const head = document.getElementsByTagName('head')[0]
   if (!head) {
     return
@@ -18,11 +18,37 @@
   const style = document.createElement('style')
   // style.type = 'text/css'
   style.innerHTML = `
+  header[role=banner] {
+    display: none !important;
+  }
+  [data-testid="primaryColumn"] {
+    margin-left: 50%;
+  }
   [data-testid="sidebarColumn"] {
     display: none !important;
   }
+
   .r-rthrr5 {
     width: 600px !important;
   }`
   head.appendChild(style)
+
+  // remove ads
+  setInterval(function() {
+    let allSpans = document.getElementsByTagName('span')
+    if (typeof allSpans !== 'undefined') {
+      for (let i = 0; i < allSpans.length; i++) {
+        if (typeof allSpans[i].innerText !== 'undefined') {
+          if (allSpans[i].innerText == 'Ad') {
+            allSpans[
+              i
+            ].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove()
+          }
+          if (allSpans[i].innerText == 'Promoted Tweet') {
+            allSpans[i].remove()
+          }
+        }
+      }
+    }
+  }, 1500)
 })()
