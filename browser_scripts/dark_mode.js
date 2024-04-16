4b997095a92a84018846284ea637afa5c6cda48c
Taylor Bartlett <taybart@gmail.com>
2024-04-16 13:43:38 -0600

    feat: dark mode userscript

---
+++ browser_scripts/dark_mode.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Dark mode
+// @namespace    https://github.com/EastSun5566
+// @version      0.0.5
+// @description  Enable dark mode with only one line of CSS
+// @author       Michael Wang
+// @license      MIT
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  const style = document.createElement('style');
+  style.textContent = `
+    html {
+      filter: invert(1) hue-rotate(180deg) contrast(0.8);
+    }
+
+    /** reverse filer for media elements */
+    img, video, picture, canvas, iframe, embed {
+      filter: invert(1) hue-rotate(180deg);
+    }
+  `;
+  document.head.appendChild(style);
+}());
