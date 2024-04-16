43840cf894d5395e17d2186e8f01e434e9afe807
Taylor Bartlett <taybart@gmail.com>
2024-04-16 13:58:36 -0600

    fix: add denylist to dark_mode

--- browser_scripts/dark_mode.js
+++ browser_scripts/dark_mode.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name         Dark mode
 // @namespace    https://github.com/EastSun5566
-// @version      0.0.5
+// @version      0.0.6
 // @description  Enable dark mode with only one line of CSS
-// @author       Michael Wang
+// @author       Michael Wang, taybart
 // @license      MIT
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
+const denylist = ["kagi.com"]
+
 (function () {
+  if (denylist.some(u => u === document.location.host)) { return }
   const style = document.createElement('style');
   style.textContent = `
     html {
