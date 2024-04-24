d0a0120e3b416f8351a002e305f803dd33c40a64
Taylor Bartlett <taybart@gmail.com>
2024-04-24 10:51:16 -0600

    fix: lock plugin to commit

--- shell/plugins/plugins.zsh
+++ shell/plugins/plugins.zsh
@@ -7,16 +7,16 @@ function use {
 }
 function usegit {
   p="$PLUGIN_DIR/${1##*/}"
-  [ ! -f $p/$2 ] && git clone "https://github.com/$1" $p
+  [ ! -f $p/$2 ] && \git clone "https://github.com/$1" $p && cd $p && git reset --hard $3 && cd ..
   source $p/$2
 }
 
 
 
-use rupa/z/master/z.sh
-use zsh-users/zsh-autosuggestions/master/zsh-autosuggestions.zsh
+use rupa/z/d37a763a6a30e1b32766fecc3b8ffd6127f8a0fd/z.sh
+use zsh-users/zsh-autosuggestions/c3d4e576c9c86eac62884bd47c01f6faed043fc5/zsh-autosuggestions.zsh
 
-use ohmyzsh/ohmyzsh/master/plugins/vi-mode/vi-mode.plugin.zsh
-use ohmyzsh/ohmyzsh/master/plugins/fzf/fzf.plugin.zsh
+use ohmyzsh/ohmyzsh/5d2d35cd1741af19553007fe0cc5324744fc58fa/plugins/vi-mode/vi-mode.plugin.zsh
+use ohmyzsh/ohmyzsh/5d2d35cd1741af19553007fe0cc5324744fc58fa/plugins/fzf/fzf.plugin.zsh
 
-usegit zsh-users/zsh-syntax-highlighting zsh-syntax-highlighting.zsh
+usegit zsh-users/zsh-syntax-highlighting zsh-syntax-highlighting.zsh e0165eaa730dd0fa321a6a6de74f092fe87630b0
