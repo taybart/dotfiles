dbf05252ad634119fb735ae1704096ab0ad6b772
Taylor Bartlett <taybart@gmail.com>
2025-03-03 15:08:48 -0700

    feat: move to llm.nvim from llama.vim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -9,16 +9,16 @@ return {
       require('utils/maps').group({ noremap = true, silent = true }, {
         {
           'n',
-          { '<c-f><left>',  ':TmuxNavigateLeft<cr>' },
-          { '<c-f><down>',  ':TmuxNavigateDown<cr>' },
-          { '<c-f><up>',    ':TmuxNavigateUp<cr>' },
+          { '<c-f><left>', ':TmuxNavigateLeft<cr>' },
+          { '<c-f><down>', ':TmuxNavigateDown<cr>' },
+          { '<c-f><up>', ':TmuxNavigateUp<cr>' },
           { '<c-f><right>', ':TmuxNavigateRight<cr>' },
         },
         {
           't',
-          { '<c-f><left>',  '<c-\\><c-n>:TmuxNavigateLeft<cr>' },
-          { '<c-f><down>',  '<c-\\><c-n>:TmuxNavigateDown<cr>' },
-          { '<c-f><up>',    '<c-\\><c-n>:TmuxNavigateUp<cr>' },
+          { '<c-f><left>', '<c-\\><c-n>:TmuxNavigateLeft<cr>' },
+          { '<c-f><down>', '<c-\\><c-n>:TmuxNavigateDown<cr>' },
+          { '<c-f><up>', '<c-\\><c-n>:TmuxNavigateUp<cr>' },
           { '<c-f><right>', '<c-\\><c-n>:TmuxNavigateRight<cr>' },
         },
       })
@@ -53,85 +53,63 @@ return {
     opts = {},
   },
 
-  {
-    'ggml-org/llama.vim',
-    init = function()
-      vim.g.llama_config = { show_info = 0 }
-    end,
-    config = function()
-      vim.api.nvim_set_hl(0, 'llama_hl_hint', { link = 'Comment' })
-      vim.cmd([[
-        silent! iunmap <buffer> <Tab>
-        " inoremap <buffer> <c-e>              <C-O>:call llama#fim_accept('line')<CR>
-        inoremap <buffer> <c-e>              <C-O>:call llama#fim_accept('full')<CR>
-      ]])
-    end,
-  },
-  {
-    'one-d-wide/lazy-patcher.nvim',
-    config = function()
-      require('lazy-patcher').setup({
-        print_logs = true,
-      })
-    end,
-    -- ft = 'lazy', -- for lazy loading
-  },
-
   -- {
-  --   'huggingface/llm.nvim',
-  --   enabled = false,
+  --   'ggml-org/llama.vim',
+  --   init = function()
+  --     vim.g.llama_config = { show_info = 0 }
+  --   end,
   --   config = function()
-  --     require('llm').setup({
-  --       backend = 'ollama',
-  --       url = 'http://unicron:11435',
-  --       -- url = 'http://localhost:11434',
-  --       -- debounce_ms = 1000,
-  --       debounce_ms = 500,
-
-  --       -- model = 'codellama:7b',
-  --       model = 'qwen2.5-coder:3b',
-  --       tokens_to_clear = { '<|endoftext|>' },
-  --       fim = {
-  --         enabled = true,
-  --         prefix = '<|fim_prefix|>',
-  --         middle = '<|fim_middle|>',
-  --         suffix = '<|fim_suffix|>',
-  --       },
-  --       request_body = {
-  --         options = {
-  --           temperature = 0.2,
-  --           top_p = 0.95,
-  --         },
-  --       },
-  --       accept_keymap = '<c-e>',
-  --       dismiss_keymap = '<c-tab>',
-  --       -- context_window = 40,
-  --       -- context_window = 1024,
-  --       context_window = 4096,
-  --       -- context_window = 8192,
-
-  --       -- starcoder
-  --       -- model = 'starcoder2:3b',
-  --       -- tokens_to_clear = { '<|endoftext|>' },
-  --       -- fim = {
-  --       --   enabled = true,
-  --       --   prefix = '<fim_prefix>',
-  --       --   middle = '<fim_middle>',
-  --       --   suffix = '<fim_suffix>',
-  --       -- },
-  --       -- model = 'bigcode/starcoder',
-  --       -- tokenizer = {
-  --       --   repository = 'bigcode/starcoder',
-  --       -- },
-
-  --       lsp = {
-  --         bin_path = vim.api.nvim_call_function('stdpath', { 'data' }) .. '/mason/bin/llm-ls',
-  --         cmd_env = { LLM_LOG_LEVEL = 'DEBUG' },
-  --       },
-  --     })
-  --     vim.keymap.set('i', '<C-l>', function()
-  --       require('llm.completion').lsp_suggest()
-  --     end, { noremap = true, silent = true })
+  --     vim.api.nvim_set_hl(0, 'llama_hl_hint', { link = 'Comment' })
+  --     vim.cmd([[
+  --       silent! iunmap <buffer> <Tab>
+  --       " inoremap <buffer> <c-e>              <C-O>:call llama#fim_accept('line')<CR>
+  --       inoremap <buffer> <c-e>              <C-O>:call llama#fim_accept('full')<CR>
+  --     ]])
   --   end,
   -- },
+  -- {
+  --   'one-d-wide/lazy-patcher.nvim',
+  --   ft = 'lazy', -- for lazy loading
+  --   config = function()
+  --     require('lazy-patcher').setup({
+  --       print_logs = true,
+  --     })
+  --   end,
+  -- },
+
+  {
+    'huggingface/llm.nvim',
+    -- enabled = false,
+    config = function()
+      require('llm').setup({
+        backend = 'openai',
+        url = 'http://localhost:8012',
+        model = 'qwen2.5-coder:3b',
+        tokens_to_clear = { '<|endoftext|>' },
+        fim = {
+          enabled = true,
+          prefix = '<|fim_prefix|>',
+          middle = '<|fim_middle|>',
+          suffix = '<|fim_suffix|>',
+        },
+        request_body = {
+          options = {
+            temperature = 0.2,
+            top_p = 0.95,
+          },
+        },
+        accept_keymap = '<c-e>',
+        dismiss_keymap = '<c-tab>',
+        -- context_window = 4096,
+        context_window = 8192,
+        lsp = {
+          bin_path = vim.api.nvim_call_function('stdpath', { 'data' }) .. '/mason/bin/llm-ls',
+          cmd_env = { LLM_LOG_LEVEL = 'DEBUG' },
+        },
+      })
+      vim.keymap.set('i', '<C-l>', function()
+        require('llm.completion').lsp_suggest()
+      end, { noremap = true, silent = true })
+    end,
+  },
 }
---
+++ nvim/scripts/llama-server.sh
@@ -0,0 +1,4 @@
+llama-server \
+    -hf ggml-org/Qwen2.5-Coder-1.5B-Q8_0-GGUF \
+    --port 8012 -ngl 99 -fa -ub 1024 -b 1024 \
+    --ctx-size 0 --cache-reuse 256
