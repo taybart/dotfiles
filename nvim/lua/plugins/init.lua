f377bbaf7dfea3d9eda265246fd8baed99043f06
Taylor Bartlett <taybart@gmail.com>
2025-10-01 09:39:29 -0600

    feat: move from Comment.nvim to ts-comment.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -38,8 +38,30 @@ return {
       height = 25,
       width = 105,
       post_open_hook = function(_, win)
+        -- necessary as per https://github.com/rmagatti/goto-preview/issues/64#issuecomment-1159069253
         vim.api.nvim_win_set_option(win, 'winhighlight', 'Normal:')
       end,
-    }, -- necessary as per https://github.com/rmagatti/goto-preview/issues/64#issuecomment-1159069253
+    },
+  },
+  {
+    'obsidian-nvim/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    opts = {
+      workspaces = {
+        {
+          name = 'personal',
+          path = '~/.notes',
+        },
+      },
+    },
   },
 }
