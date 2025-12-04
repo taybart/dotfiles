2c4aa153ea66ff86cbf8c258d7cc9cd3415749f9
Taylor Bartlett <taybart@gmail.com>
2025-12-04 08:44:35 -0700

    refact: move ftplugin to after

---
+++ nvim/after/ftplugin/dbout.lua
@@ -0,0 +1,8 @@
+-- vim.notify('dbout here buddy')
+
+-- vim.keymap.del('n', '<leader>l')
+-- vim.keymap.set('n', '<leader>l', 'f│w', { buffer = true, noremap = true, silent = true })
+--
+-- -- vim.keymap.del('n', '<leader>h')
+-- vim.keymap.set('n', '<leader>h', 'F│b', { buffer = true, noremap = true, silent = true })
+-- vim.opt.sidescrolloff = 8
