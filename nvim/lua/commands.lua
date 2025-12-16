5bd0a567d0a65b8c0ca870bc4e2a7d7985630054
Taylor Bartlett <taybart@gmail.com>
2025-12-15 18:48:34 -0700

    refact: GH and GHL commands

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -7,38 +7,44 @@ cmds.add({
   { 'Wq', 'wq' },
 })
 
-if vim.fn.executable('unix-timestamp') and false then
-  cmds.range('Date', 'unix-timestamp -u %s', { cli = true })
-elseif vim.fn.has('mac') then
-  -- cmds.one('Date', '<line1>,<line2>!xargs -I {} date -r {}', { range = true })
-  cmds.range('Date', 'date -r %s', { cli = true, print = true })
-else
-  -- cmds.one('Date', '<line1>,<line2>!xargs -I {} date -d @{}', { range = true })
-  cmds.range('Date', 'date -d @%s', { cli = true })
-end
 cmds.one('CopyPath', "let @+ = expand('%')")
 
-local function gen_github_url(include_line, branch)
+if vim.fn.executable('unix-timestamp') then
+  cmds.range('Date', 'unix-timestamp -u %s', { cli = true })
+elseif vim.fn.has('mac') then
+  cmds.range('Date', 'date -r %s', { cli = true, print = true })
+else
+  cmds.range('Date', 'date -d @%s', { cli = true })
+end
+
+-- local function gen_github_url(include_line, branch)
+local function gen_github_url(opts)
   local job = require('utils/job')
   local buf_name = vim.api.nvim_buf_get_name(0)
   -- if [No Name] open the main page
   local blob_tree = (buf_name == '') and '/tree/' or '/blob/'
 
-  if branch == nil then branch = job.run('git', { 'branch', '--show-current' }) end
+  if opts.branch == nil then opts.branch = job.run('git', { 'branch', '--show-current' }) end
 
   local url = job.run('git', { 'config', '--get', 'remote.origin.url' })
       .. blob_tree
-      .. branch
+      .. opts.branch
       .. buf_name:gsub(job.run('git', { 'rev-parse', '--show-toplevel' }):gsub('%p', '%%%1'), '')
 
-  if include_line then url = url .. '#L' .. vim.api.nvim_win_get_cursor(0)[1] end
+  if opts.include_line then url = url .. '#L' .. vim.api.nvim_win_get_cursor(0)[1] end
   return url
 end
 
-cmds.one('GH', function() require('utils/job').open({ gen_github_url() }) end)
-
-local function github_line(args)
-  local branch = args.fargs[1]
-  require('utils/job').open({ gen_github_url(true, branch) })
-end
-cmds.one('GHL', github_line, { nargs = '?' })
+cmds.add({
+  { 'GH', function() require('utils/job').open({ gen_github_url() }) end },
+  {
+    'GHL',
+    {
+      cmd = function(args)
+        local branch = args.fargs[1]
+        require('utils/job').open({ gen_github_url({ include_line = true, branch = branch }) })
+      end,
+      opts = { nargs = '?' },
+    },
+  },
+})
