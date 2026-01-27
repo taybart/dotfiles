bfde9e31028ecc5e2f3b937a0863bca6e54f81c5
taybart <taybart@gmail.com>
2026-01-27 14:38:23 -0700

    bump qwen smartness

--- nvim/scripts/llama-server.sh
+++ nvim/scripts/llama-server.sh
@@ -5,5 +5,5 @@
 #     --ctx-size 0 --cache-reuse 256
     # -hf ggml-org/Qwen2.5-Coder-3B-Q8_0-GGUF \
     # -hf ggml-org/Qwen2.5-Coder-1.5B-Q8_0-GGUF \
-llama-server --fim-qwen-1.5b-default
-# llama-server --fim-qwen-3b-default
+# llama-server --fim-qwen-1.5b-default
+llama-server --fim-qwen-3b-default
