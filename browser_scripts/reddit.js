6b86e377cac70540e944054617f653556c34ce3e
taylor <taybart@gmail.com>
2026-02-20 18:09:25 -0700

    feat: hide reddit banner script

---
+++ browser_scripts/reddit.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        reddit.com
+// @namespace   Violentmonkey Scripts
+// @match       https://old.reddit.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 2/20/2026, 6:06:53 PM
+// ==/UserScript==
+
+document.querySelector('.listingsignupbar').style.display = 'none'
